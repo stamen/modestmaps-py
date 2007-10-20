0329f6f9391babe7d8fcef10b714e8453ab02797
migurski <migurski@a23cadb0-8de1-11de-82e5-a1837a67dc72>
2007-10-20 01:16:06 +0000

    Switch to the new hilly, pretty MS Live maps
    git-svn-id: https://modestmaps.googlecode.com/svn/trunk/py@350 a23cadb0-8de1-11de-82e5-a1837a67dc72

--- ModestMaps/Microsoft.py
+++ ModestMaps/Microsoft.py
@@ -1,21 +1,21 @@
 """
 >>> p = RoadProvider()
 >>> p.getTileUrls(Coordinate(25322, 10507, 16)) #doctest: +ELLIPSIS
-('http://r....ortho.tiles.virtualearth.net/tiles/r0230102122203031.png?g=45',)
+('http://r....ortho.tiles.virtualearth.net/tiles/r0230102122203031.png?g=90&shading=hill',)
 >>> p.getTileUrls(Coordinate(25333, 10482, 16)) #doctest: +ELLIPSIS
-('http://r....ortho.tiles.virtualearth.net/tiles/r0230102033330212.png?g=45',)
+('http://r....ortho.tiles.virtualearth.net/tiles/r0230102033330212.png?g=90&shading=hill',)
 
 >>> p = AerialProvider()
 >>> p.getTileUrls(Coordinate(25322, 10507, 16)) #doctest: +ELLIPSIS
-('http://a....ortho.tiles.virtualearth.net/tiles/a0230102122203031.jpeg?g=45',)
+('http://a....ortho.tiles.virtualearth.net/tiles/a0230102122203031.jpeg?g=90&shading=hill',)
 >>> p.getTileUrls(Coordinate(25333, 10482, 16)) #doctest: +ELLIPSIS
-('http://a....ortho.tiles.virtualearth.net/tiles/a0230102033330212.jpeg?g=45',)
+('http://a....ortho.tiles.virtualearth.net/tiles/a0230102033330212.jpeg?g=90&shading=hill',)
 
 >>> p = HybridProvider()
 >>> p.getTileUrls(Coordinate(25322, 10507, 16)) #doctest: +ELLIPSIS
-('http://h....ortho.tiles.virtualearth.net/tiles/h0230102122203031.jpeg?g=45',)
+('http://h....ortho.tiles.virtualearth.net/tiles/h0230102122203031.jpeg?g=90&shading=hill',)
 >>> p.getTileUrls(Coordinate(25333, 10482, 16)) #doctest: +ELLIPSIS
-('http://h....ortho.tiles.virtualearth.net/tiles/h0230102033330212.jpeg?g=45',)
+('http://h....ortho.tiles.virtualearth.net/tiles/h0230102033330212.jpeg?g=90&shading=hill',)
 """
 
 from Core import Coordinate
@@ -42,15 +42,15 @@ class AbstractProvider(IMapProvider):
 
 class RoadProvider(AbstractProvider):
     def getTileUrls(self, coordinate):
-        return ('http://r%d.ortho.tiles.virtualearth.net/tiles/r%s.png?g=45' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
+        return ('http://r%d.ortho.tiles.virtualearth.net/tiles/r%s.png?g=90&shading=hill' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
 
 class AerialProvider(AbstractProvider):
     def getTileUrls(self, coordinate):
-        return ('http://a%d.ortho.tiles.virtualearth.net/tiles/a%s.jpeg?g=45' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
+        return ('http://a%d.ortho.tiles.virtualearth.net/tiles/a%s.jpeg?g=90&shading=hill' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
 
 class HybridProvider(AbstractProvider):
     def getTileUrls(self, coordinate):
-        return ('http://h%d.ortho.tiles.virtualearth.net/tiles/h%s.jpeg?g=45' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
+        return ('http://h%d.ortho.tiles.virtualearth.net/tiles/h%s.jpeg?g=90&shading=hill' % (random.randint(0, 3), self.getZoomString(self.sourceCoordinate(coordinate))),)
 
 if __name__ == '__main__':
     import doctest
