430e8a9ac340a590471c751d065708dbc7881a8a
migurski <migurski@a23cadb0-8de1-11de-82e5-a1837a67dc72>
2010-04-08 19:19:56 +0000

    Turns out that writing a setup script is the easiest thing in the world.
    git-svn-id: https://modestmaps.googlecode.com/svn/trunk/py@857 a23cadb0-8de1-11de-82e5-a1837a67dc72

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ModestMaps',
+      version='1.0',
+      description='Modest Maps python port',
+      author='Michal Migurski',
+      url='http://modestmaps.com',
+      packages=['ModestMaps'])
